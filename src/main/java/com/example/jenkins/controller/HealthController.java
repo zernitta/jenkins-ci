3edc0ab70152017e731d01863e2bb8e14178e2fa
宇东 胡 <yudong.hu@dianrong.com>
2020-06-12 16:40:02 +0800

    Initial Commit

---
+++ src/main/java/com/example/jenkins/controller/HealthController.java
@@ -0,0 +1,26 @@
+package com.example.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+//@Component
+public class HealthController {
+
+
+
+
+
+
+    @GetMapping(value = "/health/check")
+    public String healthCheck() {
+//        Report report = new Report();
+//        report.setN2(0D);
+        return "SUCCESS";
+//        return 1000L;
+    }
+
+
+
+}
